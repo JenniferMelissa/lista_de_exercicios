9df93ca411037d541a25cd6455c9bb89c8e2483a
jennifermel <jennifermelelissa8503@gmail.com>
2024-09-04 16:38:42 -0300

    operador ternario exercicio 2

--- 02_exercicio.py
+++ 02_exercicio.py
@@ -1,9 +1,7 @@
-peso = input('Informe o peso em gramas do recém-nascido: ').replace(',','.')
-peso_float = float(peso)
+try:
+    peso = int(input('Informe o peso em gramas do recém-nascido: ').replace(',','.'))
 
-
-if peso_float < 2500:
-    print('Está abaixo do peso. O recém-nascido precisará ficar internado.')
-
-else:
-    print('O recém nascido está com o peso normal. Está liberado para ir para casa.')
+    #operador ternario
+    print('O bebê está liverado.' if peso >= 2500 else 'O bebê precisa ser internado.')
+except Exception as e:
+    print(f'Não foi possível calcular o peso do bebê: {e}.')
